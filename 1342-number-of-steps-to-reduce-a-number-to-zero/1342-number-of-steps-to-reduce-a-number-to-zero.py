f99b25dc3d7249e047aea5a069baf71683c677b9
Prince Rawat <prince.rawat603340@gmail.com>
2026-08-24 20:54:23 +0530

    Time: 4 ms (3.98%), Space: 12.4 MB (16.9%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        count=0
+        temp=num
+        while temp!=0:
+            if temp%2==0:
+                temp=temp/2
+                count+=1
+            else:
+                temp=temp-1
+                count+=1
+        return count
+            
