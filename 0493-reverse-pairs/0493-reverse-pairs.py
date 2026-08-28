8afaa512f66b05e41f017c562fd4cde232d15061
Prince Rawat <prince.rawat603340@gmail.com>
2026-08-28 21:11:35 +0530

    Time: 1227 ms (58.11%), Space: 17.4 MB (66.72%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,38 +1,49 @@
 class Solution(object):
+    def merge(self,nums,l,m,r):
+        i=l
+        j=m+1
+        temp=[]
+        count=0
+
+        for a in range(l, m + 1):
+            while j <= r and nums[a] > 2 * nums[j]:
+                j += 1
+
+            count += j - (m + 1)
+        j=m+1
+        
+        while i<=m and j<=r:
+            if nums[i]<=nums[j]:
+                temp.append(nums[i])
+                i+=1
+            else:
+                temp.append(nums[j])
+                j+=1
+        while i<=m:
+            temp.append(nums[i])
+            i+=1
+        while j<=r:
+            temp.append(nums[j])
+            j+=1
+        nums[l:r+1]=temp
+        return count
+
+
+    def mergesort(self,nums,strt,end):
+        if strt>=end:
+            return 0
+        mid=(strt+end)//2
+        leftcount=self.mergesort(nums,strt,mid)
+        rightcount=self.mergesort(nums,mid+1,end)
+        count= self.merge(nums,strt,mid,end)
+        return count+leftcount+rightcount
     def reversePairs(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        def merge_sort(left,right):
-            if left>=right:
-                return 0
-            mid=(left+right)//2
-            count=0
-            count+=merge_sort(left,mid)
-            count+=merge_sort(mid+1,right)
-            j=mid+1
-            for i in range(left,mid+1):
-                while j<=right and nums[i]>2* nums[j]:
-                    j+=1
-                count+=j-(mid+1)
-            temp=[]
-            i=left
-            j=mid+1
-            while i<=mid and j <=right:
-                if nums[i]<=nums[j]:
-                    temp.append(nums[i])
-                    i+=1
-                else :
-                    temp.append(nums[j])
-                    j+=1
-            while i<=mid:
-                temp.append(nums[i])
-                i+=1
-            while j<=right:
-                temp.append(nums[j])
-                j+=1
-            for k in range(len(temp)):
-                nums[left+k]=temp[k]
-            return count
-        return merge_sort(0,len(nums)-1)
+        strt=0
+        end=len(nums)-1
+        count=self.mergesort(nums,strt,end)
+        return count
+        
