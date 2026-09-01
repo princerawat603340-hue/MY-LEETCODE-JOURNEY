8338e1f10431840dab348437b2866256c3b31a59
Prince Rawat <prince.rawat603340@gmail.com>
2026-09-01 06:28:06 +0530

    Time: 3 ms (32.52%), Space: 12.5 MB (72%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        arr=s.split()
+        ans=''
+        for i in range(len(arr)-1,-1,-1):
+            if arr[i]!=' ':
+                ans+=arr[i]
+                ans+=' '
+        ans=ans[0:-1]
+        return ans
+
