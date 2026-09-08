980bbb5fa8f7cb5979e87952f829aa68fcf927f5
Prince Rawat <prince.rawat603340@gmail.com>
2026-09-08 21:00:49 +0530

    Time: 0 ms (100%), Space: 12.2 MB (93.2%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<1000:
+            return 0
+        else:
+            return n-999
+            
