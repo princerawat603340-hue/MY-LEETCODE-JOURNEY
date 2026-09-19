1eb87b16d45c0fe178fcf8ad03e9bbd220c5a890
Prince Rawat <prince.rawat603340@gmail.com>
2026-09-19 07:38:36 +0530

    Time: 0 ms (100%), Space: 12.4 MB (59.17%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,46 +1,42 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
 
-        carry=0
-        ans=[]
-        l1node=l1
-        l2node=l2
-        while (l1node is not None )and(l2node is not None) :
-            ans.append(((l1node.val+l2node.val+carry)%10))
-            carry=(l1node.val+l2node.val+carry)//10
-           
-            l1node=l1node.next
-            l2node=l2node.next
-            
-        while l1node is not None:
-            ans.append((l1node.val+carry)%10) 
-            carry=(l1node.val+carry)//10
-            
-            l1node=l1node.next
-        while l2node is not None:
-            ans.append((l2node.val+carry)%10)
-            carry=(l2node.val+carry)//10
-            l2node=l2node.next
-        if carry!=0:
-            ans.append(carry)
-            
-        head=ListNode(ans[0])
-        curr=head
-        for i in range(1,len(ans)):
-            curr.next=ListNode(ans[i]) 
-            curr=curr.next
-        
+        head = l1
+        carry = 0
+
+        while l1 and l2:
+
+            total = l1.val + l2.val + carry
+
+            l1.val = total % 10
+            carry = total // 10
+
+            prev = l1
+
+            l1 = l1.next
+            l2 = l2.next
+        if l2:
+            prev.next = l2
+
+            while l2:
+                total = l2.val + carry
+
+                l2.val = total % 10
+                carry = total // 10
+
+                prev = l2
+                l2 = l2.next
+        elif l1:
+            while l1:
+                total = l1.val + carry
+
+                l1.val = total % 10
+                carry = total // 10
+
+                prev = l1
+                l1 = l1.next
+        if carry:
+            prev.next = ListNode(carry)
+
         return head
-    
-           
-           
+
